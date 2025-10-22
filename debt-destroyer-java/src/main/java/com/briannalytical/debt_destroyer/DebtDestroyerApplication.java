c1afc9eb218215ebfccf14cea8610fdbf890ac7e
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-22 17:14:45 -0400

    java project dependencies configured

---
+++ debt-destroyer-java/src/main/java/com/briannalytical/debt_destroyer/DebtDestroyerApplication.java
@@ -0,0 +1,13 @@
+package com.briannalytical.debt_destroyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtDestroyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtDestroyerApplication.class, args);
+	}
+
+}
---
+++ debt-destroyer-java/src/test/java/com/briannalytical/debt_destroyer/DebtDestroyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briannalytical.debt_destroyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtDestroyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
